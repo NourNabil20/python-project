acebc5f499596f8c9358940f455902828463c134
Your Name <you@example.com>
2022-11-06 15:45:33 +0200

    Multiply 2 numbers

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ print("Hello world")
 def add2NumberS(n1,n2):
     return n1 + n2
 def sub2NumberS(n1,n2):
-    return n1 - n2    
+    return n1 - n2  
+def mul2NumberS(n1,n2):
+    return n1 * n2          
